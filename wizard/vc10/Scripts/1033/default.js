c05695abca35ce4af9a323c9002dfb19904d9c35
Hendrik Polczynski <hendrikpolczyn@gmail.com>
2012-10-03 18:21:04 +0200

    tabs and uppercase project name

--- wizard/vc10/Scripts/1033/default.js
+++ wizard/vc10/Scripts/1033/default.js
@@ -1,245 +1,249 @@
+/* Plugin SDK - for licensing and copyright see license.txt */
 
 function OnFinish(selProj, selObj)
 {
     try 
     {
+        var strProjectPath = wizard.FindSymbol('PROJECT_PATH');
+        var strProjectName = wizard.FindSymbol('PROJECT_NAME');
 
-		var strProjectPath = wizard.FindSymbol('PROJECT_PATH');
-		var strProjectName = wizard.FindSymbol('PROJECT_NAME');
+        // Safe Project Name
+        strProjectName = strProjectName.replace(/[^\w\s]/gi, '');
+        wizard.AddSymbol('PROJECT_NAME_SAFE', strProjectName);
 
-		strProjectName = strProjectName.replace(/[^\w\s]/gi, '');
-		wizard.AddSymbol('PROJECT_NAME_SAFE', strProjectName);
+        // Safe Project Name in Uppercase
+        wizard.AddSymbol('PROJECT_NAME_SAFE_UPPERCASE', strProjectName.toUpperCase());
 
-		var strTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
-		wizard.RenderTemplate(strTemplatePath + "\\Plugin_Settings.props", "Plugin_Settings.props", true);
+        var strTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
+        wizard.RenderTemplate(strTemplatePath + "\\Plugin_Settings.props", "Plugin_Settings.props", true);
 
-		selProj = CreateCustomProject(strProjectName, strProjectPath);
-		AddConfig(selProj, strProjectName);
-		SetupFilters(selProj);
+        selProj = CreateCustomProject(strProjectName, strProjectPath);
+        AddConfig(selProj, strProjectName);
+        SetupFilters(selProj);
 
-		var InfFile = CreateCustomInfFile();
-		AddFilesToCustomProj(selProj, strProjectName, strProjectPath, InfFile);
-		PchSettings(selProj);
-		InfFile.Delete();
+        var InfFile = CreateCustomInfFile();
+        AddFilesToCustomProj(selProj, strProjectName, strProjectPath, InfFile);
+        PchSettings(selProj);
+        InfFile.Delete();
 
-		selProj.Object.Save();
-	}
-	catch(e)
-	{
-		if (e.description.length != 0)
-			SetErrorInfo(e);
-		return e.number
-	}
+        selProj.Object.Save();
+    }
+    catch(e)
+    {
+        if (e.description.length != 0)
+            SetErrorInfo(e);
+        return e.number
+    }
 }
 
 function CreateCustomProject(strProjectName, strProjectPath)
 {
-	try
-	{
-		var strProjTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
-		var strProjTemplate = '';
-		strProjTemplate = strProjTemplatePath + '\\default.vcxproj';
+    try
+    {
+        var strProjTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
+        var strProjTemplate = '';
+        strProjTemplate = strProjTemplatePath + '\\default.vcxproj';
 
-		var Solution = dte.Solution;
-		var strSolutionName = "";
-		if (wizard.FindSymbol("CLOSE_SOLUTION"))
-		{
-			Solution.Close();
-			strSolutionName = wizard.FindSymbol("VS_SOLUTION_NAME");
-			if (strSolutionName.length)
-			{
-				var strSolutionPath = strProjectPath.substr(0, strProjectPath.length - strProjectName.length);
-				Solution.Create(strSolutionPath, strSolutionName);
-			}
-		}
+        var Solution = dte.Solution;
+        var strSolutionName = "";
+        if (wizard.FindSymbol("CLOSE_SOLUTION"))
+        {
+            Solution.Close();
+            strSolutionName = wizard.FindSymbol("VS_SOLUTION_NAME");
+            if (strSolutionName.length)
+            {
+                var strSolutionPath = strProjectPath.substr(0, strProjectPath.length - strProjectName.length);
+                Solution.Create(strSolutionPath, strSolutionName);
+            }
+        }
 
-		var strProjectNameWithExt = '';
-		strProjectNameWithExt = strProjectName + '.vcxproj';
+        var strProjectNameWithExt = '';
+        strProjectNameWithExt = strProjectName + '.vcxproj';
 
-		var oTarget = wizard.FindSymbol("TARGET");
-		var prj;
-		if (wizard.FindSymbol("WIZARD_TYPE") == vsWizardAddSubProject)  // vsWizardAddSubProject
-		{
-			var prjItem = oTarget.AddFromTemplate(strProjTemplate, strProjectNameWithExt);
-			prj = prjItem.SubProject;
-		}
-		else
-		{
-			prj = oTarget.AddFromTemplate(strProjTemplate, strProjectPath, strProjectNameWithExt);
-		}
-		var fxtarget = wizard.FindSymbol("TARGET_FRAMEWORK_VERSION");
-		if (fxtarget != null && fxtarget != "")
-		{
-		    fxtarget = fxtarget.split('.', 2);
-		    if (fxtarget.length == 2)
-			prj.Object.TargetFrameworkVersion = parseInt(fxtarget[0]) * 0x10000 + parseInt(fxtarget[1])
-		}
-		return prj;
-	}
-	catch(e)
-	{
-		throw e;
-	}
+        var oTarget = wizard.FindSymbol("TARGET");
+        var prj;
+        if (wizard.FindSymbol("WIZARD_TYPE") == vsWizardAddSubProject)  // vsWizardAddSubProject
+        {
+            var prjItem = oTarget.AddFromTemplate(strProjTemplate, strProjectNameWithExt);
+            prj = prjItem.SubProject;
+        }
+        else
+        {
+            prj = oTarget.AddFromTemplate(strProjTemplate, strProjectPath, strProjectNameWithExt);
+        }
+        var fxtarget = wizard.FindSymbol("TARGET_FRAMEWORK_VERSION");
+        if (fxtarget != null && fxtarget != "")
+        {
+            fxtarget = fxtarget.split('.', 2);
+            if (fxtarget.length == 2)
+            prj.Object.TargetFrameworkVersion = parseInt(fxtarget[0]) * 0x10000 + parseInt(fxtarget[1])
+        }
+        return prj;
+    }
+    catch(e)
+    {
+        throw e;
+    }
 }
 
 function AddFilters(proj)
 {
-	try
-	{
-		// Add the folders to your project
-		var group = proj.Object.AddFilter("Source Files");
-		group.Filter = ".cpp;.cxx;.c";
+    try
+    {
+        // Add the folders to your project
+        var group = proj.Object.AddFilter("Source Files");
+        group.Filter = ".cpp;.cxx;.c";
 
-		group = proj.Object.AddFilter("Header");
-		gorup.Filter = ".h;.hpp";
-	}
-	catch(e)
-	{
-		throw e;
-	}
+        group = proj.Object.AddFilter("Header");
+        gorup.Filter = ".h;.hpp";
+    }
+    catch(e)
+    {
+        throw e;
+    }
 }
 
 function AddConfig(proj, strProjectName)
 {
-	try
-	{
+    try
+    {
 
-	    var config = proj.Object.Configurations('Debug');
-	    config.ConfigurationType = typeDynamicLibrary;
+        var config = proj.Object.Configurations('Debug');
+        config.ConfigurationType = typeDynamicLibrary;
 
-		var CLTool = config.Tools('VCCLCompilerTool');
-		// TODO: Add compiler settings
+        var CLTool = config.Tools('VCCLCompilerTool');
+        // TODO: Add compiler settings
 
-		var LinkTool = config.Tools('VCLinkerTool');
-		// TODO: Add linker settings
+        var LinkTool = config.Tools('VCLinkerTool');
+        // TODO: Add linker settings
 
-		config = proj.Object.Configurations('Release');
-		config.ConfigurationType = typeDynamicLibrary;
+        config = proj.Object.Configurations('Release');
+        config.ConfigurationType = typeDynamicLibrary;
 
-		var CLTool = config.Tools('VCCLCompilerTool');
-		// TODO: Add compiler settings
+        var CLTool = config.Tools('VCCLCompilerTool');
+        // TODO: Add compiler settings
 
-		var LinkTool = config.Tools('VCLinkerTool');
-		// TODO: Add linker settings
+        var LinkTool = config.Tools('VCLinkerTool');
+        // TODO: Add linker settings
 
-		/*config = proj.Object.Configurations('Profile')
+        /*config = proj.Object.Configurations('Profile')
 
-		var CLTool = config.Tools('VCCLCompilerTool');
-		// TODO: Add compiler settings
+        var CLTool = config.Tools('VCCLCompilerTool');
+        // TODO: Add compiler settings
 
-		var LinkTool = config.Tools('VCLinkerTool');
-		// TODO: Add linker settings*/
-	}
-	catch(e)
-	{
-		throw e;
-	}
+        var LinkTool = config.Tools('VCLinkerTool');
+        // TODO: Add linker settings*/
+    }
+    catch(e)
+    {
+        throw e;
+    }
 }
 
 function PchSettings(proj)
 {
-	// TODO: specify pch settings
+    // TODO: specify pch settings
 }
 
 function DelFile(fso, strWizTempFile)
 {
-	try
-	{
-		if (fso.FileExists(strWizTempFile))
-		{
-			var tmpFile = fso.GetFile(strWizTempFile);
-			tmpFile.Delete();
-		}
-	}
-	catch(e)
-	{
-		throw e;
-	}
+    try
+    {
+        if (fso.FileExists(strWizTempFile))
+        {
+            var tmpFile = fso.GetFile(strWizTempFile);
+            tmpFile.Delete();
+        }
+    }
+    catch(e)
+    {
+        throw e;
+    }
 }
 
 function CreateCustomInfFile()
 {
-	try
-	{
-		var fso, TemplatesFolder, TemplateFiles, strTemplate;
-		fso = new ActiveXObject('Scripting.FileSystemObject');
+    try
+    {
+        var fso, TemplatesFolder, TemplateFiles, strTemplate;
+        fso = new ActiveXObject('Scripting.FileSystemObject');
 
-		var TemporaryFolder = 2;
-		var tfolder = fso.GetSpecialFolder(TemporaryFolder);
-		var strTempFolder = tfolder.Drive + '\\' + tfolder.Name;
+        var TemporaryFolder = 2;
+        var tfolder = fso.GetSpecialFolder(TemporaryFolder);
+        var strTempFolder = tfolder.Drive + '\\' + tfolder.Name;
 
-		var strWizTempFile = strTempFolder + "\\" + fso.GetTempName();
+        var strWizTempFile = strTempFolder + "\\" + fso.GetTempName();
 
-		var strTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
-		var strInfFile = strTemplatePath + '\\Templates.inf';
-		wizard.RenderTemplate(strInfFile, strWizTempFile);
+        var strTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
+        var strInfFile = strTemplatePath + '\Templates.inf';
+        wizard.RenderTemplate(strInfFile, strWizTempFile);
 
-		var WizTempFile = fso.GetFile(strWizTempFile);
-		return WizTempFile;
-	}
-	catch(e)
-	{
-		throw e;
-	}
+        var WizTempFile = fso.GetFile(strWizTempFile);
+        return WizTempFile;
+    }
+    catch(e)
+    {
+        throw e;
+    }
 }
 
 function GetTargetName(strName, strProjectName)
 {
-	try
-	{
-		// TODO: set the name of the rendered file based on the template filename
-		var strTarget = strName;
+    try
+    {
+        // TODO: set the name of the rendered file based on the template filename
+        var strTarget = strName;
 
-		if (strName == 'readme.txt')
-			strTarget = 'ReadMe.txt';
+        if (strName == 'readme.txt')
+            strTarget = 'ReadMe.txt';
 
-		if (strName == 'sample.txt')
-			strTarget = 'Sample.txt';
+        if (strName == 'sample.txt')
+            strTarget = 'Sample.txt';
 
-		return strTarget;
-	}
-	catch(e)
-	{
-		throw e;
-	}
+        return strTarget;
+    }
+    catch(e)
+    {
+        throw e;
+    }
 }
 
 function AddFilesToCustomProj(proj, strProjectName, strProjectPath, InfFile)
 {
-	try
-	{
-		var projItems = proj.ProjectItems
+    try
+    {
+        var projItems = proj.ProjectItems
 
-		var strTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
+        var strTemplatePath = wizard.FindSymbol('TEMPLATES_PATH');
 
-		var strTpl = '';
-		var strName = '';
+        var strTpl = '';
+        var strName = '';
 
-		var strTextStream = InfFile.OpenAsTextStream(1, -2);
-		while (!strTextStream.AtEndOfStream)
-		{
-			strTpl = strTextStream.ReadLine();
-			if (strTpl != '')
-			{
-				strName = strTpl;
-				var strTarget = GetTargetName(strName, strProjectName);
-				var strTemplate = strTemplatePath + '\\' + strTpl;
-				var strFile = strProjectPath + '\\' + strTarget;
+        var strTextStream = InfFile.OpenAsTextStream(1, -2);
+        while (!strTextStream.AtEndOfStream)
+        {
+            strTpl = strTextStream.ReadLine();
+            if (strTpl != '')
+            {
+                strName = strTpl;
+                var strTarget = GetTargetName(strName, strProjectName);
+                var strTemplate = strTemplatePath + '\\' + strTpl;
+                var strFile = strProjectPath + '\\' + strTarget;
 
-				var bCopyOnly = false;  //"true" will only copy the file from strTemplate to strTarget without rendering/adding to the project
-				var strExt = strName.substr(strName.lastIndexOf("."));
-				if(strExt==".bmp" || strExt==".ico" || strExt==".gif" || strExt==".rtf" || strExt==".css")
-					bCopyOnly = true;
-				wizard.RenderTemplate(strTemplate, strFile, bCopyOnly);
-				proj.Object.AddFile(strFile);
-			}
-		}
-		strTextStream.Close();
-	}
-	catch(e)
-	{
-		throw e;
-	}
+                var bCopyOnly = false;  //"true" will only copy the file from strTemplate to strTarget without rendering/adding to the project
+                var strExt = strName.substr(strName.lastIndexOf("."));
+                if(strExt==".bmp" || strExt==".ico" || strExt==".gif" || strExt==".rtf" || strExt==".css")
+                    bCopyOnly = true;
+                wizard.RenderTemplate(strTemplate, strFile, bCopyOnly);
+                proj.Object.AddFile(strFile);
+            }
+        }
+        strTextStream.Close();
+    }
+    catch(e)
+    {
+        throw e;
+    }
 }
 
 
