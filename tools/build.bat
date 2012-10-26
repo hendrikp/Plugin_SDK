b420a4daf0fda744849f6c0f2e5179b6518f3aa3
Hendrik Polczynski <hendrikpolczyn@gmail.com>
2012-10-26 20:04:48 +0200

    build script wip

---
+++ tools/build.bat
@@ -0,0 +1,9 @@
+call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+
+MSBuild "../project/PluginManager.vcxproj" /t:Rebuild /p:Configuration=Release
+
+call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x64
+
+MSBuild "../project/PluginManager.vcxproj" /t:Rebuild /p:Configuration=Release
+
+pause
