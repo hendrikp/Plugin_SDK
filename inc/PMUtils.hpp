11aea774fd995707d0c0b120a1ef3806f7cbfa30
Hendrik Polczynski <hendrikpolczyn@gmail.com>
2012-10-28 17:14:19 +0100

    type utils + delay trigger

--- inc/PMUtils.hpp
+++ inc/PMUtils.hpp
@@ -1,5 +1,10 @@
 /* CE3 Plugin Manager - for licensing and copyright see license.txt */
 
+/**
+* @file
+* Utility functions that can be used directly by anyone
+*/
+
 #pragma once
 
 #include <StdAfx.h>
@@ -13,8 +18,23 @@
 #undef WIN32_LEAN_AND_MEAN
 #endif
 
+#include <sstream>
+
 #ifndef PATH_SEPERATOR
 #define PATH_SEPERATOR "\\"
+#endif
+
+#ifndef SAFESTR
+#define SAFESTR(x) (((const char*)x)?((const char*)x):"") //!< Helper to avoid NULL str problems
+#endif
+
+#undef STRING_TRUE
+#define STRING_TRUE "true" //!< true value
+#undef STRING_FALSE
+#define STRING_FALSE "false" //!< false value
+
+#ifndef BOOLSTR
+#define BOOLSTR(x) (x?STRING_TRUE:STRING_FALSE) //!< Helper to convert boolean values
 #endif
 
 namespace PluginManager
@@ -207,8 +227,19 @@ namespace PluginManager
     /**
     * @internal
     * @brief delayed function type
+    * @param pData data for the delayed call
+    * This is the delayed call
     */
     typedef void ( *tDelayedCall )( void* );
+
+    /**
+    * @internal
+    * @brief delay call trigger type
+    * Delay Trigger type functions can be used to monitor dynamic conditions
+    * @param pDataTrigger data for the delay trigger
+    * On true this executes the delayed call
+    */
+    typedef bool ( *tDelayedCallTrigger )( void* );
 
     /**
     * @internal
@@ -226,33 +257,73 @@ namespace PluginManager
                 eDT_None = 0, //!< initialization
                 eDT_Frames = 1, //!< frame based delay
                 eDT_Seconds = 2, //!< time based delay
+                eDT_Trigger = 3, //!< trigger based delay
                 eDT_Default = eDT_Frames
             };
 
         protected:
             tDelayedCall m_pFunc; //!< function pointer delayed function
+            tDelayedCall m_pFuncCleanup; //!< cleanup function
+
             void* m_pData; //!< data for delayed function
             eDelayType m_eDT; //!< type of delay
             float m_fLeft; //!< delay amount left
 
+            tDelayedCallTrigger m_pFuncTrigger; //!< function pointer delay trigger function
+            tDelayedCall m_pFuncTriggerCleanup; //!< trigger cleanup function
+            void* m_pDataTrigger; //!< data for delay trigger function
+
         private:
             CallDelay() {};
+            //CallDelay( CallDelay& copy ) {};
 
         public:
+
+            /**
+            * @brief ~CallDelay destructor
+            * Calls Cleanup functions if present
+            */
+            ~CallDelay()
+            {
+                if ( m_pFuncCleanup )
+                {
+                    m_pFuncCleanup( m_pData );
+                    m_pData = NULL;
+                }
+
+                if ( m_pFuncTriggerCleanup )
+                {
+                    m_pFuncTriggerCleanup( m_pDataTrigger );
+                    m_pDataTrigger = NULL;
+                }
+            };
+
             /**
             * @internal
             * @brief CallDelay constructor
+            * The Delayed function and Trigger function can be used separated or together
             * @param pFunc function pointer of delayed function
+            * @param pFuncCleanup function pointer that cleans up the data when the delayed call is canceled or finished
             * @param pData Data for delayed function
             * @param fDelay delay amount
             * @param eType delay type
+            * @param pFuncTrigger function pointer delay trigger function
+            * @param pFuncTriggerCleanup function pointer that cleans up the data when the delayed call is canceled or finished
+            * @param pDataTrigger Data for delay trigger function
             */
-            CallDelay( tDelayedCall pFunc, void* pData = NULL, float fDelay = 1.0f, eDelayType eType = eDT_Default )
+            CallDelay( tDelayedCall pFunc = NULL, tDelayedCall pFuncCleanup = NULL, void* pData = NULL, float fDelay = 1.0f, eDelayType eType = eDT_Default, tDelayedCallTrigger pFuncTrigger = NULL, tDelayedCall pFuncTriggerCleanup = NULL, void* pDataTrigger = NULL )
             {
+                // Standard
                 m_pFunc = pFunc;
+                m_pFuncCleanup = pFuncCleanup;
                 m_pData = pData;
                 m_eDT = eType;
                 m_fLeft = fDelay;
+
+                // Trigger type
+                m_pFuncTrigger = pFuncTrigger;
+                m_pDataTrigger = pDataTrigger;
+                m_pFuncTriggerCleanup = pFuncTriggerCleanup;
             }
 
             /**
@@ -275,8 +346,30 @@ namespace PluginManager
 
                 if ( m_fLeft <= 0 || m_eDT == eDT_None )
                 {
-                    m_pFunc( m_pData );
+
+                    if ( m_pFunc )
+                    {
+                        m_pFunc( m_pData );
+                    }
+
                     return true;
+                }
+
+                if ( m_eDT == eDT_Trigger )
+                {
+                    bool bRet = true;
+
+                    if ( m_pFuncTrigger )
+                    {
+                        bRet = m_pFuncTrigger( m_pData );
+
+                        if ( bRet && m_pFunc )
+                        {
+                            m_pFunc( m_pData );
+                        }
+                    }
+
+                    return bRet;
                 }
 
                 return false;
@@ -290,20 +383,25 @@ namespace PluginManager
     class CallDelayQueue
     {
         private:
-            std::list<CallDelay> m_queue;
+            std::map<string, std::list<std::shared_ptr<CallDelay>>> m_queue;
 
         public:
             /*
             * @internal
             * @brief Delay a function
+            * @param sFilter custom filter for remove control
             * @param pFunc function pointer of delayed function
+            * @param pFuncCleanup function pointer that cleans up the data when the delayed call is canceled or finished
             * @param pData Data for delayed function
             * @param fDelay delay amount
             * @param eType delay type
+            * @param pFuncTrigger function pointer delay trigger function
+            * @param pFuncTriggerCleanup function pointer that cleans up the data when the delayed call is canceled or finished
+            * @param pDataTrigger Data for delay trigger function
             */
-            void DelayFunction( tDelayedCall pFunc, void* pData = NULL, float fDelay = 1.0f, CallDelay::eDelayType eType = CallDelay::eDT_Default )
+            void DelayFunction( const char* sFilter = NULL, tDelayedCall pFunc = NULL, tDelayedCall pFuncCleanup = NULL, void* pData = NULL, float fDelay = 1.0f, CallDelay::eDelayType eType = CallDelay::eDT_Default, tDelayedCallTrigger pFuncTrigger = NULL, tDelayedCall pFuncTriggerCleanup = NULL, void* pDataTrigger = NULL )
             {
-                m_queue.push_back( CallDelay( pFunc, pData, fDelay, eType ) );
+                m_queue[ SAFESTR( sFilter ) ].push_back( std::shared_ptr<CallDelay>( new CallDelay( pFunc, pFuncCleanup, pData, fDelay, eType, pFuncTrigger, pFuncTriggerCleanup, pDataTrigger ) ) );
             }
 
             /**
@@ -315,20 +413,59 @@ namespace PluginManager
             {
                 bool bFinished = false;
 
-                for ( auto qIter = m_queue.begin(); qIter != m_queue.end(); ++qIter )
+                // for each filter
+                for ( auto mIter = m_queue.begin(); mIter != m_queue.end(); ++mIter )
                 {
-                    bFinished = ( *qIter ).Advance( fDeltaTime );
+                    auto& lQueue = mIter->second;
 
-                    // Execution of this function is finished
-                    if ( bFinished )
+                    // for each delayed call of this filter
+                    for ( auto qIter = lQueue.begin(); qIter != lQueue.end(); ++qIter )
                     {
-                        qIter = m_queue.erase( qIter );
+                        // Do the processing
+                        bFinished = ( *qIter )->Advance( fDeltaTime );
 
-                        if ( qIter == m_queue.end() )
+                        // Execution of this function is finished
+                        if ( bFinished )
+                        {
+                            // Clean it up and fix iterator
+                            qIter = lQueue.erase( qIter );
+
+                            if ( qIter == lQueue.end() )
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    // if the filter is empty, clean it up
+                    if ( !lQueue.size() )
+                    {
+                        mIter = m_queue.erase( mIter );
+
+                        if ( mIter == m_queue.end() )
                         {
                             break;
                         }
+
+                        break;
                     }
+                }
+            }
+
+            /**
+            * @brief Cancels registered delayed calls
+            * @param sFilter only delete these delayed calls (if NULL then everything is deleted)
+            */
+            void Cancel( const char* sFilter = NULL )
+            {
+                if ( !sFilter )
+                {
+                    m_queue.clear();
+                }
+
+                else
+                {
+                    m_queue.erase( sFilter );
                 }
             }
     };
@@ -336,7 +473,7 @@ namespace PluginManager
     /**
     * @internal
     * @brief Delayed Console Commands
-    * @param sCommand to execute and delete[]
+    * @param sCommand to execute
     */
     static void _DelayedConsoleCommand( void* sCommand )
     {
@@ -346,7 +483,18 @@ namespace PluginManager
             {
                 gEnv->pConsole->ExecuteString( ( const char* )sCommand );
             }
+        }
+    }
 
+    /**
+    * @internal
+    * @brief Delayed Console Commands cleanup
+    * @param sCommand to delete[]
+    */
+    static void _CleanupDelayedConsoleCommand( void* sCommand )
+    {
+        if ( sCommand )
+        {
             delete [] ( const char* )( sCommand );
         }
     }
@@ -357,10 +505,11 @@ namespace PluginManager
     * Since the built in defer didn't work as expected
     * @param qDelayer Delay Queue that handles this command
     * @param sCommand the console command
+    * @param sFilter filter for the console command (can be used to cancel the command) default is "consolecommand"
     * @param fDelay delay amount
     * @param eType delay type
     */
-    static void DelayConsoleCommand( CallDelayQueue& qDelayer, string sCommand, float fDelay = 1, CallDelay::eDelayType eType = CallDelay::eDT_Default )
+    static void DelayConsoleCommand( CallDelayQueue& qDelayer, string sCommand, const char* sFilter = NULL, float fDelay = 1, CallDelay::eDelayType eType = CallDelay::eDT_Default, tDelayedCallTrigger pFuncTrigger = NULL, tDelayedCall pFuncTriggerCleanup = NULL, void* pDataTrigger = NULL )
     {
         if ( sCommand.length() > 0 )
         {
@@ -370,7 +519,102 @@ namespace PluginManager
             strncpy( sCmd, sCommand.c_str(), nLen );
             sCmd[nLen - 1] = 0;
 
-            qDelayer.DelayFunction( &_DelayedConsoleCommand, sCmd, fDelay, eType );
+            const char* sFilterC = ( sFilter && sFilter[0] ) ? sFilter : "consolecommand";
+
+            qDelayer.DelayFunction( sFilter, &_DelayedConsoleCommand, &_CleanupDelayedConsoleCommand, sCmd, fDelay, eType, pFuncTrigger, pFuncTriggerCleanup, pDataTrigger );
         }
+    }
+
+    static int stringExplode( string tear_me, string cut, std::vector<string>& store )
+    {
+        size_t start = 0;
+        int len = 0;
+        int i = 0;
+        store.clear();
+
+        len = cut.length();
+
+        if ( tear_me.find( cut ) == string::npos )
+        {
+            return -1;
+        }
+
+        while ( ( start = tear_me.find( cut ) ) != string::npos )
+        {
+            store.push_back( tear_me.substr( 0, start ) );
+            tear_me.erase( 0, start + len );
+            i++;
+        }
+
+        store.push_back( tear_me );
+
+        return i;
+    };
+
+    template<typename tDataType>
+    static tDataType ParseString( const char* c )
+    {
+        tDataType value;
+        std::istringstream tmp_conv( c );
+        tmp_conv >> value;
+
+        return value;
+    };
+
+    template<>
+    static Vec3 ParseString<Vec3>( const char* c )
+    {
+        Vec3 v;
+
+        std::istringstream tmp_conv;
+        std::vector<string> temp;
+
+        stringExplode( c, ",", temp );
+        assert( temp.size() == 3 );
+
+        // x-Value
+        if ( temp.size() >= 1 )
+        {
+            tmp_conv.str( temp[0].c_str() );
+            tmp_conv >> v.x;
+        }
+
+        // y-Value
+        if ( temp.size() >= 2 )
+        {
+            tmp_conv.str( temp[1].c_str() );
+            tmp_conv >> v.y;
+        }
+
+        // z-Value
+        if ( temp.size() >= 3 )
+        {
+            tmp_conv.str( temp[2].c_str() );
+            tmp_conv >> v.z;
+        }
+
+        return v;
+    };
+
+    template<>
+    static bool ParseString<bool>( const char* c )
+    {
+        bool value = false;
+
+        // "1" and "true" are interpreted as true, "0" and "false" as false
+        string sValue = c;
+        sValue.MakeLower();
+
+        if ( sValue == "1" || sValue == STRING_TRUE )
+        {
+            value = true;
+        }
+
+        else if ( sValue == "0" || sValue == STRING_FALSE )
+        {
+            value = false;
+        }
+
+        return value;
     }
 }
