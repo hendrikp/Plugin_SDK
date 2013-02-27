e0dbc1cc57c78d8b1171f956fa27bf7ce0e2dbe6
Hendrik Polczynski <hendrikpolczyn@gmail.com>
2013-02-27 22:38:01 +0100

    version 3.4.5 support
    now handles version changes fully automatic with rebuilt (as long as
    interfaces dont change :P)

--- tools/stylecode.bat
+++ tools/stylecode.bat
@@ -1,5 +1,7 @@
+:: This is an prebuilt script part of the Plugin SDK
 @echo off
 
+cd %~dp0
 cmd /C "_stylehelper.bat ..\src cpp"
 cmd /C "_stylehelper.bat ..\src h"
 cmd /C "_stylehelper.bat ..\src hpp"
@@ -9,5 +11,3 @@ cmd /C "_stylehelper.bat ..\inc hpp"
 cmd /C "_stylehelper.bat ..\wizard cpp"
 cmd /C "_stylehelper.bat ..\wizard h"
 cmd /C "_stylehelper.bat ..\wizard hpp"
-
-pause
